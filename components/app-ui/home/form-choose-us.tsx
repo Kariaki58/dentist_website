6b4688273099cd397c3dc4258d17222bf1d70e01
Kariaki58 <kariakistephen809@gmail.com>
2025-02-28 07:21:47 +0100

    fixed eslint error

--- components/app-ui/home/form-choose-us.tsx
+++ components/app-ui/home/form-choose-us.tsx
@@ -3,10 +3,10 @@ import PhoneDialer from "./PhoneDialer";
 export default function FormChooseUs() {
     return (
         <aside className="bg-white p-10 w-96 rounded-sm shadow-orange-500 shadow-sm">
-            <h2 className="text-3xl font-bold mb-3">Don't Hesitate to Call Us</h2>
+            <h2 className="text-3xl font-bold mb-3">Don&apos;t Hesitate to Call Us</h2>
             <p className="mb-5 text-gray-700">
                 Have questions or need assistance? Our team is here to help! 
-                Reach out to us anytime, and we'll be happy to assist you.
+                Reach out to us anytime, and we&apos;ll be happy to assist you.
             </p>
             <div className="space-y-5">
                 <h3 className="flex justify-between">
